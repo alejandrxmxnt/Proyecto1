f38329e2bed598ca1a9f4dc8d935aabae23e0442
alejandrxmxnt <amont9483@gmail.com>
2023-08-12 18:58:01 -0400

    Template de proyecto lado administracion

--- bootstrap/js/custom.js
+++ bootstrap/js/custom.js
@@ -1,143 +1,35 @@
-(function () {
+$(function() {
+    "use strict";
 
-	'use strict'
+    $(".preloader").fadeOut();
+    // this is for close icon when navigation open in mobile view
+    $(".nav-toggler").on('click', function() {
+        $("#main-wrapper").toggleClass("show-sidebar");
+        $(".nav-toggler i").toggleClass("ti-menu");
+    });
+    $(".search-box a, .search-box .app-search .srh-btn").on('click', function() {
+        $(".app-search").toggle(200);
+        $(".app-search input").focus();
+    });
 
+    // ============================================================== 
+    // Resize all elements
+    // ============================================================== 
+    $("body, .page-wrapper").trigger("resize");
+    $(".page-wrapper").delay(20).show();
+    
+    //****************************
+    /* This is for the mini-sidebar if width is less then 1170*/
+    //**************************** 
+    var setsidebartype = function() {
+        var width = (window.innerWidth > 0) ? window.innerWidth : this.screen.width;
+        if (width < 1170) {
+            $("#main-wrapper").attr("data-sidebartype", "mini-sidebar");
+        } else {
+            $("#main-wrapper").attr("data-sidebartype", "full");
+        }
+    };
+    $(window).ready(setsidebartype);
+    $(window).on("resize", setsidebartype);
 
-	AOS.init({
-		duration: 800,
-		easing: 'slide',
-		once: true
-	});
-
-	var preloader = function() {
-
-		var loader = document.querySelector('.loader');
-		var overlay = document.getElementById('overlayer');
-
-		function fadeOut(el) {
-			el.style.opacity = 1;
-			(function fade() {
-				if ((el.style.opacity -= .1) < 0) {
-					el.style.display = "none";
-				} else {
-					requestAnimationFrame(fade);
-				}
-			})();
-		};
-
-		setTimeout(function() {
-			fadeOut(loader);
-			fadeOut(overlay);
-		}, 200);
-	};
-	preloader();
-
-	var tinyslider = function() {
-
-		var slider = document.querySelectorAll('.features-slider');
-		var postSlider = document.querySelectorAll('.post-slider');
-		var testimonialSlider = document.querySelectorAll('.testimonial-slider');
-		
-		
-		
-		if ( slider.length> 0 ) {
-			var tnsSlider = tns({
-				container: '.features-slider',
-				mode: 'carousel',
-				speed: 700,
-				items: 3,
-				// center: true,
-				gutter: 30,
-				loop: false,
-				edgePadding: 80,
-				controlsPosition: 'bottom',
-				// navPosition: 'bottom',
-				nav: false,
-				// autoplay: true,
-				// autoplayButtonOutput: false,
-				controlsContainer: '#features-slider-nav',
-				responsive: {
-					0: {
-						items: 1
-					},
-					700: {
-						items: 2
-					},
-					900: {
-						items: 3
-					}
-				}
-			});
-		}
-
-		if ( postSlider.length> 0 ) {
-			var tnsPostSlider = tns({
-				container: '.post-slider',
-				mode: 'carousel',
-				speed: 700,
-				items: 3,
-				// center: true,
-				gutter: 30,
-				loop: true,
-				edgePadding: 10,
-				controlsPosition: 'bottom',
-				navPosition: 'bottom',
-				nav: true,
-				autoplay: true,
-				autoplayButtonOutput: false,
-				controlsContainer: '#post-slider-nav',
-				responsive: {
-					0: {
-						items: 1
-					},
-					700: {
-						items: 2
-					},
-					900: {
-						items: 3
-					}
-				}
-			});
-		}
-
-		if ( testimonialSlider.length> 0 ) {
-			var tnsTestimonialSlider = tns({
-				container: '.testimonial-slider',
-				mode: 'carousel',
-				speed: 700,
-				items: 1,
-				// center: true,
-				gutter: 30,
-				loop: true,
-				edgePadding: 10,
-				controlsPosition: 'bottom',
-				navPosition: 'bottom',
-				nav: true,
-				autoplay: true,
-				autoplayButtonOutput: false,
-				controlsContainer: '#testimonial-slider-nav',
-				controls: false,
-				responsive: {
-					0: {
-						items: 1
-					},
-					700: {
-						items: 1
-					},
-					900: {
-						items: 1
-					}
-				}
-			});
-		}
-
-		
-	}
-	tinyslider();
-
-	var lightboxVideo = GLightbox({
-		selector: '.glightbox'
-	});
-
-
-})()
+});
