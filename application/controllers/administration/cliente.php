740544f524a3dbf1286dcfddf780141db0f91ce7
alejandrxmxnt <amont9483@gmail.com>
2023-08-13 18:55:03 -0400

    Se cargan las vistas y se carga la data para la insersion

--- application/controllers/administration/cliente.php
+++ application/controllers/administration/cliente.php
@@ -19,6 +19,28 @@
 
         public function agregar(){
             //mostrar un formulario para agregar nuevo Cliente.
-            $lista=$this->cliente_model->agregarbd();
+            //este formulario va estar una vista
+            $this->load->view('view_administration/admidesing/headboard');
+            $this->load->view('view_administration/admidesing/menuSuperior');
+            $this->load->view('view_administration/admidesing/menuLateral');
+            $this->load->view('view_administration/cliente_formulario');//direccion de vista.
+            $this->load->view('view_administration/admidesing/foot');
+        }
+
+        public function agregarbd()
+        {
+            //  atributo.  BDD = name de formulario
+            $data['nombre']=$_POST['nombre'];
+            $data['primerApellido']=$_POST['apellido1'];
+            $data['segundoApellido']=$_POST['apellido2'];
+            $data['ciNit']=$_POST['cinit'];
+            $data['telefono']=$_POST['celular'];
+            $data['direccion']=$_POST['direccion'];
+            $data['razonSocial']=$_POST['razonSocial'];
+
+            $this->cliente_model->agregarcliente($data); //hasta ahi ya guarda en BDD
+
+            redirect('administration/cliente/index','refresh');//con el refresh refrescamos de forma forsoza si es que hay problema
+
         }
     }
