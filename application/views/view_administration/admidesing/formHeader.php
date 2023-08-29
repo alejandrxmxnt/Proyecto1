8f9bed86069816aed274312fb1457e46b09332df
alejandrxmxnt <amont9483@gmail.com>
2023-08-29 17:42:31 -0400

    Se desarrollo una cabecera distinta para cada tipo de formulario | para evitar problemas con los .css de cada formulario

---
+++ application/views/view_administration/admidesing/formHeader.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>MUEBLERIA LARA</title>
+    <link rel="icon" type="Usuario" sizes="16x16" href="<?php echo base_url();?>img/logos/logomuebleria.jpeg">
+    <link href="<?php echo base_url();?>bootstrap/css/style.min.css" rel="stylesheet">
+    <!-- CABECERA DEDICADA PARA FORMULARIOS -->
+    <link rel="stylesheet" href="<?php echo base_url();?>bootstrap/css/form/style.css">
+    <!-- Imagen de fondo -->
+    <style>
+        /* Imagen de fondo */
+        body {
+            background-image: url('<?php echo base_url();?>img/logos/fondo.jpeg');
+            background-repeat: no-repeat;
+            background-size: 100% 100%;
+            backdrop-filter: blur(3px);  /* borroso */
+            transition: 0.5s;
+            opacity: 1;
+        }
+
+    </style>
+</head>
+
+<body class="bodyy">
+    <!-- ============================================================== -->
+    <!-- Preloader - style you can find in spinners.css -->
+    <!-- ============================================================== -->
+    <div class="preloader">
+        <div class="lds-ripple">
+            <div class="lds-pos"></div>
+            <div class="lds-pos"></div>
+        </div>
+    </div>
+
+        <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <div id="main-wrapper" data-layout="vertical" data-navbarbg="skin5" data-sidebartype="full"
+        data-sidebar-position="absolute" data-header-position="absolute" data-boxed-layout="full">
+        <!-- ============================================================== -->
+        <!-- Topbar header - style you can find in pages.scss -->
+        <!-- ============================================================== -->
+        <header class="topbar" data-navbarbg="skin5">
