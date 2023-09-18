1cf65073a54cb6d6cf341fd03a939669430ee5ea
alejandrxmxnt <amont9483@gmail.com>
2023-09-17 23:34:08 -0400

    CRUDs y direccionamiento de las imagenes a una carpeta - tiene fallos corregir

--- application/controllers/administration/producto.php
+++ application/controllers/administration/producto.php
@@ -21,7 +21,7 @@
         public function agregar()
         {
             //mostar un formulario(vista) para agregar nuevo usuario.
-            $this->load->view('view_administration/admidesing/formHeader');
+            $this->load->view('view_administration/admidesing/productoFormHeader');
             $this->load->view('view_administration/admidesing/menuSuperior');
             $this->load->view('view_administration/admidesing/menuLateral');
             $this->load->view('view_administration/producto_formulario');
@@ -30,17 +30,37 @@
 
         public function agregarbd()
         {
-            //  atributo.  BDD = name de formulario
-            $data['nombre']=$_POST['nombre'];
-            $data['descripcion']=$_POST['descripcion'];
-            $data['precioUnitario']=$_POST['precioUnitario'];
-            $data['stock']=$_POST['stock'];
-            $data['codigo']=$_POST['codigo'];
-            $data['foto']=$_POST['foto'];
-            $data['idCategoria']=$_POST['idCategoria'];
+            $nombrearchivo=$_POST['nombre'].".jpg";
+            $config['upload_path']='./uploads/productos/'; //direccion donde se almacenara el producto
+            $config['file_name']=$nombrearchivo;
+            $direccion="./uploads/productos/".$nombrearchivo;
 
-            $this->producto_model->agregarproducto($data); //hasta ahi ya guarda en BDD
+            if(file_exists($direccion)){
+                unlink($direccion);//si existe el archivo se remplaza por esta
+            }
+            $config['allowed_types']='jpg|jpeg';//tipos de archivos que voy a permitir
+            $this->load->library('upload',$config); //carga de libreria
 
+            if(!$this->upload->do_upload())//si no se logra subir la foto
+            {
+                $data['error']=$this->upload->display_errors();//almacena todos los errores que salgan
+                //archivos pesados
+                //no hay permisos
+            }
+            else{
+                $data['nombre']=$_POST['nombre'];
+                $data['descripcion']=$_POST['descripcion'];
+                $data['precioUnitario']=$_POST['precioUnitario'];
+                $data['stock']=$_POST['stock'];
+                $data['codigo']=$_POST['codigo'];
+
+                $data['foto']=$nombrearchivo;
+                //$databdd['foto']=$nombrearchivo;
+
+                $this->producto_model->agregarproducto($data); //hasta ahi ya guarda en BDD
+                $this->upload->data();//subir la imagen
+            }
+            
             redirect('administration/producto/index','refresh');//con el refresh refrescamos de forma forsoza si es que hay problema
 
         }
@@ -52,7 +72,7 @@
             $data['infoproducto']=$this->producto_model->recuperarproductos($idproducto); //asi hago llegar el idcliente al modelo
 
             //cargar la vista
-            $this->load->view('view_administration/admidesing/headboard');
+            $this->load->view('view_administration/admidesing/productoFormHeader');
             $this->load->view('view_administration/admidesing/menuSuperior');
             $this->load->view('view_administration/admidesing/menuLateral');
             $this->load->view('view_administration/producto_modificar',$data);//ahi llega la informacion.
