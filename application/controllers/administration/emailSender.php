279a1033a9487a0e7b4d0c738824acde423fce66
alejandrxmxnt <amont9483@gmail.com>
2023-09-17 23:21:46 -0400

    PHPmailer estructura base para envio de correos electronicos

---
+++ application/controllers/administration/emailSender.php
@@ -0,0 +1,51 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+class EmailSender{
+    public function enviarDatos($correo,$nombreUsuario,$contraseniaSegura)
+    {
+        $mail = new PHPMailer(true); //para las escepciones
+
+        try {
+            //Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = 'correomuevleria@gmail.com';                     //SMTP username
+            $mail->Password   = 'jqdihuepqdkvwiap';                               //SMTP password
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+            //Recipients
+            $mail->setFrom('correomuevleria@gmail.com', 'ENVIO DE CONTRASEÑA');
+            $mail->addAddress($correo, 'CORREO ELECTRONICO');     //Add a recipient
+            /*
+            $mail->addAddress('ellen@example.com');               //Name is optional
+            $mail->addReplyTo('info@example.com', 'Information');       */
+            $mail->addCC('adralemont@gmail.com'); //MODIFICACION PENDIENTE
+            //$mail->addBCC('bcc@example.com');
+
+            //Attachments
+            // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = 'CLAVES DE ACCESO';
+            $mail->Body    = 'Contraseña de acceso: <b>'$nombreUsuario'</b> <br>Contraseña de acceso: <b>'$contraseniaSegura'</b> <br>';
+            //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            $mail->send();
+            echo 'Envio de contraseña';
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        }
+    }
+}
