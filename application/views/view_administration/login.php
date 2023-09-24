28323f8994f25f9b1bcf18d8b8c565ffd057f5b9
alejandrxmxnt <amont9483@gmail.com>
2023-09-24 18:40:38 -0400

    Mejoras en login aplicacion de validaciones y mejora en la parte de interfaz

--- application/views/view_administration/login.php
+++ application/views/view_administration/login.php
@@ -62,7 +62,15 @@ echo form_close();
 			    </footer><!--//app-auth-footer-->	
 		    </div><!--//flex-column-->   
 	    </div><!--//auth-main-col-->
-	    <div class="col-12 col-md-5 col-lg-6 h-100 auth-background-col">
+	    <div class="col-12 col-md-5 col-lg-6 h-100 auth-background-col" 
+		style="
+			background-image: url('<?php echo base_url();?>img/logos/logomuebleria.jpeg');
+			background-repeat: no-repeat;
+            background-size: 100% 100%;
+            backdrop-filter: blur(3px);  /* borroso */
+            transition: 0.5s;
+            opacity: 1;
+		">
 		    <div class="auth-background-holder">
 		    </div>
 		    <div class="auth-background-mask"></div>
@@ -70,8 +78,8 @@ echo form_close();
 			    <div class="d-flex flex-column align-content-end h-100">
 				    <div class="h-100"></div>
 				    <div class="overlay-content p-3 p-lg-4 rounded">
-					    <h5 class="mb-3 overlay-title">Explore Portal Admin Template</h5>
-					    <div>Portal is a free Bootstrap 5 admin dashboard template. You can download and view the template license <a href="https://themes.3rdwavemedia.com/bootstrap-templates/admin-dashboard/portal-free-bootstrap-admin-dashboard-template-for-developers/">here</a>.</div>
+					    <h5 class="mb-3 overlay-title">Muebleria Lara.</h5>
+					    <div>Tienda de comercio de muebles. Mira nuestros productos, conoce mas de notros. <a href="https://themes.3rdwavemedia.com/bootstrap-templates/admin-dashboard/portal-free-bootstrap-admin-dashboard-template-for-developers/">visitanos</a>.</div>
 				    </div>
 				</div>
 		    </div><!--//auth-background-overlay-->
