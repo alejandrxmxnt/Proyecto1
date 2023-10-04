989189e884cbb478880d5c1aa341705d6afa4bfd
alejandrxmxnt <amont9483@gmail.com>
2023-10-04 13:56:09 -0400

    creacion de formulario para el cambio de contraseña

---
+++ application/views/view_administration/admidesing/updatePasswordFormHeader.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>MUEBLERIA LARA</title>
+    <!--
+    <link rel="canonical" href="https://www.wrappixel.com/templates/ample-admin-lite/" /> -->
+    <!-- Favicon icon -->
+    <link rel="icon" type="Usuario" sizes="16x16" href="<?php echo base_url();?>img/logos/logomuebleria.jpeg">
+    <!-- Custom CSS 
+    <link href="<?php echo base_url();?>bootstrap/plugins/bower_components/chartist/dist/chartist.min.css" rel="stylesheet">
+    -->
+<!--
+    <link rel="stylesheet" href="<?php echo base_url();?>bootstrap/plugins/bower_components/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.css">
+     Custom CSS -->
+    <link href="<?php echo base_url();?>bootstrap/css/style.min.css" rel="stylesheet">
+    <!--
+    <link rel="stylesheet" href="<?php echo base_url();?>bootstrap/css/form/clienteForm.css">
+    -->
+
+
+    <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"> 
+	<link rel="stylesheet" href="<?php echo base_url();?>bootstrap/css/form/estilosUpdatePassword.css">
+
+
+
+    <!-- Imagen de fondo -->
+    <style>
+        /* Imagen de fondo 
+        body {
+            background-image: url('<?php echo base_url();?>img/logos/fondo.jpeg');
+            background-repeat: no-repeat;
+            background-size: 100% 100%;
+            backdrop-filter: blur(3px); 
+            transition: 0.5s;
+            opacity: 1;
+        }
+*/
+    </style>
+</head>
+
+<body class="bodyy">
+    <!-- ============================================================== -->
+    <!-- Preloader - style you can find in spinners.css -->
+    <!-- ============================================================== -->
+    <div class="preloader">
+        <div class="lds-ripple">
+            <div class="lds-pos"></div>
+            <div class="lds-pos"></div>
+        </div>
+    </div>
+
+        <!-- ============================================================== -->
+    <!-- Main wrapper - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <div id="main-wrapper" data-layout="vertical" data-navbarbg="skin5" data-sidebartype="full"
+        data-sidebar-position="absolute" data-header-position="absolute" data-boxed-layout="full">
+        <!-- ============================================================== -->
+        <!-- Topbar header - style you can find in pages.scss -->
+        <!-- ============================================================== -->
+        <header class="topbar" data-navbarbg="skin5">
