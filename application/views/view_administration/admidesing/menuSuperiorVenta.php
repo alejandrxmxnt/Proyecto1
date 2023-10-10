78a37a9c5477328fd9e0344ff13851f3be59d0a1
alejandrxmxnt <amont9483@gmail.com>
2023-10-10 12:46:33 -0400

    desarrollo de cabecera de venta traen otras extenciones de css

---
+++ application/views/view_administration/admidesing/menuSuperiorVenta.php
@@ -0,0 +1,54 @@
+<nav class="navbar top-navbar navbar-expand-md navbar-dark">
+                <div class="navbar-header" data-logobg="skin6" style="/*
+                    background-image: url('<?php echo base_url();?>img/menu/texturamadera.jpg'); 
+                    background-repeat: no-repeat; 
+                    background-size: 100% 100%; */
+                    background-color: white;
+                    ">
+                    <!-- ============================================================== -->
+                    <!-- Logo -->
+                    <!-- ============================================================== -->
+                    <a class="navbar-brand" href="<?php echo base_url();?>index.php/administration/usuario/index">
+                        <!-- Logo icon -->
+                        <b class="logo-icon">
+                            <!-- Dark Logo icon -->
+                            <img src="<?php echo base_url();?>img/logos/titulo3dd.png" style="border-radius: 50px;" alt="homepage" />
+                        </b>
+                        <!--End Logo icon -->
+                        <!-- Logo text -->
+                    </a>
+                    <!-- ============================================================== -->
+                    <!-- End Logo -->
+                    <!-- ============================================================== -->
+                    <!-- ============================================================== -->
+                    <!-- toggle and nav items -->
+                    <!-- ============================================================== -->
+                    <a class="nav-toggler waves-effect waves-light text-dark d-block d-md-none"
+                        href="javascript:void(0)"><i class="ti-menu ti-close"></i></a>
+                </div>
+                <!-- ============================================================== -->
+                <!-- End Logo -->
+                <!-- ============================================================== -->
+                <div class="navbar-collapse collapse" id="navbarSupportedContent" data-navbarbg="skin5" style="background-image: url('<?php echo base_url();?>img/menu/texturamadera.jpg'); background-repeat: no-repeat; 
+                    background-size: 100% 100%;">
+                   
+                    <!-- ============================================================== -->
+                    <!-- Right side toggle and nav items -->
+                    <!-- ============================================================== -->
+                    <ul class="navbar-nav ms-auto d-flex align-items-center">
+                        <!-- ============================================================== -->
+                        <!-- User profile and search -->
+                        <!-- ============================================================== -->
+                        <li>
+                            <a class="profile-pic" href="#">
+                                <img src="<?php echo base_url();?>img/logos/logomuebleria.jpeg" alt="user-img" width="36"
+                                    class="img-circle"><span class="text-white font-medium">
+                                        Usuario: <?php echo $this->session->userdata('login'); ?>
+                                    </span></a>
+                        </li>
+                        <!-- ============================================================== -->
+                        <!-- User profile and search -->
+                        <!-- ============================================================== -->
+                    </ul>
+                </div>
+            </nav>
