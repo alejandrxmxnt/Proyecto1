982a24d8cbe04124180e1e3e4a71f5128a0f50b9
alejandrxmxnt <amont9483@gmail.com>
2023-10-19 18:15:39 -0400

    Limpieza de Codigo - Eliminar archivos basura

--- application/views/view_administration/formventa.php
+++ application/views/view_administration/formventa.php
@@ -1,9 +1,6 @@
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
-<!-- Bootstrap 
-<link href="<?php echo base_url() ?>assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
--->
 <!--scrip para el funcionamiento necesario-->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
